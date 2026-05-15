41e6e100c27c1699d920fd344c1319d5475608d6
Kike <kike.cerv@gmail.com>
2026-05-14 21:27:29 -0600

    Fix usuarios app migrations

--- config/settings.py
+++ config/settings.py
@@ -51,7 +51,7 @@ CSRF_TRUSTED_ORIGINS = [
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get("SECRET_KEY", "test123")
+SECRET_KEY = os.environ["SECRET_KEY"]
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get("DEBUG") == "True"
