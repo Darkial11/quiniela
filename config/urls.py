ccb813dc451b3090c425be59022a1a99ce8c876b
Kike <kike.cerv@gmail.com>
2026-05-23 18:08:13 -0600

    Actualizacion dk

--- config/urls.py
+++ config/urls.py
@@ -18,7 +18,7 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('luki-panel/', admin.site.urls),
     path('', include('quiniela.urls')),
     path('', include('usuarios.urls')),
 ]
