cdac37a4232f98245a060ce5e7e7a15b45e459bb
Kike <kike.cerv@gmail.com>
2026-06-17 14:31:08 -0600

    Actualizacion Ranking

--- quiniela/admin.py
+++ quiniela/admin.py
@@ -7,6 +7,30 @@ from .models import (
     Jornada
 )
 
+
+@admin.register(Pronostico)
+class PronosticoAdmin(admin.ModelAdmin):
+
+    list_display = (
+        'user',
+        'get_jornada',
+        'partido',
+        'seleccion',
+    )
+
+    list_filter = (
+        'partido__jornada',
+    )
+
+    search_fields = (
+        'user__username',
+    )
+
+    def get_jornada(self, obj):
+        return obj.partido.jornada
+
+    get_jornada.short_description = 'Jornada'
+
+
 admin.site.register(Partido)
-admin.site.register(Pronostico)
 admin.site.register(Jornada)
